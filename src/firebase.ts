7580dae9d8fff715ca6cef27863ac0d128f938ea
Hans Tognon <learningway403@gmail.com>
2022-03-05 15:36:44 +0100

    init

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp({
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+});
+
+export default app;
