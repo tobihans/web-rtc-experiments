ab6269fadf6d94b5c4e881b5fd7075ebc73dec35
Hans Tognon <learningway403@gmail.com>
2022-03-05 15:43:27 +0100

    dev: intellisense for env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_FB_API_KEY: string
+  readonly VITE_FB_AUTH_DOMAIN: string
+  readonly VITE_FB_PROJECT_ID: string
+  readonly VITE_FB_STORAGE_BUCKET: string
+  readonly VITE_FB_MSG_SENDER_ID: string
+  readonly VITE_FB_APP_ID: string
+
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
